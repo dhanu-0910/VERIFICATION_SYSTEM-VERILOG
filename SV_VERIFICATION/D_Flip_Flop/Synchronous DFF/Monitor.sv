a12458f828f67d6e135c03865a430ee0b9f052de
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-18 11:03:44 +0530

    Create Monitor.sv

---
+++ SV_VERIFICATION/D_Flip_Flop/Synchronous DFF/Monitor.sv
@@ -0,0 +1,25 @@
+class monitor;
+  
+  transaction tr;
+  mailbox #(transaction) m2s;
+  virtual d_if vif;
+ 
+  function new(mailbox #(transaction) m2s,virtual d_if vif);
+    this.m2s = m2s;
+    this.vif  = vif;
+  endfunction
+ 
+  task run();
+ 
+    repeat (30) begin
+      tr = new();
+      @(vif.mon_cb);
+      tr.rst = vif.mon_cb.rst;
+      tr.d = vif.mon_cb.d;
+      tr.q = vif.mon_cb.q;
+      m2s.put(tr);
+      $display("[MONITOR]   rst=%0b d=%0b q=%0b", tr.rst, tr.d, tr.q);
+    end
+  endtask
+ 
+endclass
