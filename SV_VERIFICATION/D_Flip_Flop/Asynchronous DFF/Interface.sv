a8c02594df15a8f62f15d2fcac820e47cf27311d
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-19 17:56:51 +0530

    Create Interface.sv

---
+++ SV_VERIFICATION/D_Flip_Flop/Asynchronous DFF/Interface.sv
@@ -0,0 +1,18 @@
+interface d_if (input logic clk,  logic rst);
+ 
+
+  logic d;
+  logic q;
+ 
+  clocking drv_cb @(posedge clk);
+    default input #1step output #0;
+    input  q;
+    output rst, d;
+  endclocking
+ 
+  clocking mon_cb @(posedge clk);
+    default input #1step output #0;
+    input q, rst, d;
+  endclocking
+ 
+endinterface
