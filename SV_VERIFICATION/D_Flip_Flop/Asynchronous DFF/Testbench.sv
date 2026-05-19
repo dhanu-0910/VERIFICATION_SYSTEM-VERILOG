777ba3ab9cb1ffbddb6783958b123711f3bee762
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-19 17:56:17 +0530

    Create Testbench.sv

---
+++ SV_VERIFICATION/D_Flip_Flop/Asynchronous DFF/Testbench.sv
@@ -0,0 +1,41 @@
+`include "d_if.sv"
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "environment.sv"
+ 
+module d_ff_tb;
+ 
+  logic clk;
+ 
+  d_if vif (.clk(clk));
+ 
+  environment env;
+ 
+  d_ff dut (
+    .clk (clk),
+    .rst (vif.rst),
+    .d   (vif.d),
+    .q   (vif.q)
+  );
+ 
+  initial begin
+    clk = 1'b0;
+    forever #10 clk = ~clk;
+  end
+ 
+  initial begin
+    $dumpfile("d_ff_tb.vcd");
+    $dumpvars(0, d_ff_tb);
+    $display("*********************************************************");
+    $display("*               VERIFICATION OF D_FLIPFLOP              * ");
+    $display("*********************************************************");
+    env = new(vif);
+    env.run();
+    #1000;
+    $finish;
+  end
+ 
+endmodule
