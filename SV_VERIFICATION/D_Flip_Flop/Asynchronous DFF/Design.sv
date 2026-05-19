e99473358d1f951f2ef2d6d70a836bd418b5852c
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-19 17:53:23 +0530

    Create Design.sv

---
+++ SV_VERIFICATION/D_Flip_Flop/Asynchronous DFF/Design.sv
@@ -0,0 +1,8 @@
+module d_ff (input  logic clk,input  logic d,input  logic rst,output logic q);
+  always @(posedge clk or posedge rst) begin
+    if (rst)
+      q <= 1'b0;
+    else
+      q <= d;
+  end
+endmodule
