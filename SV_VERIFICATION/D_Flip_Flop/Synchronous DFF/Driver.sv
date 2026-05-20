aa209eae6b5700749b37ded3723b5445a8c49927
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-20 11:10:06 +0530

    Update Driver.sv

--- SV_VERIFICATION/D_Flip_Flop/Synchronous DFF/Driver.sv
+++ SV_VERIFICATION/D_Flip_Flop/Synchronous DFF/Driver.sv
@@ -2,12 +2,10 @@ class driver;
   transaction tr;
   mailbox #(transaction) g2d;
   virtual d_if vif;
-  event done;
  
-  function new(mailbox #(transaction) g2d,virtual d_if vif,event done);
+  function new(mailbox #(transaction) g2d,virtual d_if vif);
     this.g2d = g2d;
     this.vif  = vif;
-    this.done = done;
   endfunction
  
   task run();
@@ -15,10 +13,9 @@ class driver;
     repeat (30) begin
       g2d.get(tr);
       @(vif.drv_cb);
-      vif.drv_cb.rst <= tr.rst;
-      vif.drv_cb.d   <= tr.d;
+      vif.rst <= tr.rst;
+      vif.d   <= tr.d;
       $display("[DRIVER]    rst=%0b d=%0b", tr.rst, tr.d);
-      ->done;
     end
 
   endtask
