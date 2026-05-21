c8b510c837965e50d7307dbf13a886a80bb13274
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-21 09:24:35 +0530

    Create Interface.sv

---
+++ SV_VERIFICATION/Counter/Johnson_counter/Interface.sv
@@ -0,0 +1,17 @@
+interface jc_if(input clk);
+  
+  logic rst;
+  logic [3:0]q;
+  
+  clocking drv_cb @(negedge clk);
+    default input #1 output #0;
+    output rst;
+  endclocking
+  
+  clocking mon_cb @(posedge clk);
+    default input #1 output #0;
+    input q;
+    input rst;
+  endclocking
+  
+endinterface
