c27842ae0d4b695c4efe23df392b083bd2f60c99
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-22 17:02:45 +0530

    Create Environment.sv

---
+++ SV_VERIFICATION/Counter/Decade_counter/Environment.sv
@@ -0,0 +1,36 @@
+class environment;
+  
+  generator g;
+  driver d;
+  monitor m;
+  scoreboard s;
+  
+  mailbox #(transaction) g2d;
+  mailbox #(transaction) m2s;
+  
+  virtual dc_if vif;
+  
+  function new(virtual dc_if vif);
+    this.vif=vif;
+    g2d=new();
+    m2s=new();
+    g=new(g2d);
+    d=new(g2d,vif);
+    m=new(m2s,vif);
+    s=new(m2s);
+  endfunction
+  
+  task run();
+    fork
+      g.run();
+      d.run();
+      m.run();
+      s.run();
+    join
+  endtask
+endclass
+
+  
+    
+  
+  
