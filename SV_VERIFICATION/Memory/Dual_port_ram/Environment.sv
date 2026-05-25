280b99f0237698cfba5a3fd6276ae48948816091
dhanu-0910 <dhanushiya952@gmail.com>
2026-05-25 12:10:59 +0530

    Create Environment.sv

---
+++ SV_VERIFICATION/Memory/Dual_port_ram/Environment.sv
@@ -0,0 +1,31 @@
+class environment;
+  generator  gen;
+  driver     drv;
+  monitor    mon;
+  scoreboard scb;
+ 
+  mailbox #(transaction) g2d;
+  mailbox #(transaction) m2s;
+ 
+  virtual dp_if vif;
+ 
+  function new(virtual dp_if vif);
+    this.vif = vif;
+    g2d = new();
+    m2s = new();
+    gen = new(g2d);
+    drv = new(g2d, vif);
+    mon = new(m2s, vif);
+    scb = new(m2s);
+  endfunction
+ 
+  task run();
+    fork
+      gen.run();
+      drv.run();
+      mon.run();
+      scb.run();
+    join
+  endtask
+endclass
+ 
